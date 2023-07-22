b6efc91916325d9c651669d95884859d0abad216
布尔 <zhengyanchong.zyc@alibaba-inc.com>
2023-07-22 12:52:48 +0800

    fix: model download

--- src/code/source-code/download-model2nas/index.js
+++ src/code/source-code/download-model2nas/index.js
@@ -18,7 +18,7 @@ exports.handler = async (_event, _context, callback) => {
     await downloads(fileUrl, {
       dest: downloadDir,
       filename,
-      extract: true,
+      extract: false,
     });
     callback(null, 'download success');
   }
