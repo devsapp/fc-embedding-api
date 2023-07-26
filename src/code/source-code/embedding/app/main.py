1dde1c8197c09e287bb9bb4cbd0a16e7f8ee8978
hanxie <hanxie.wq@alibaba-inc.com>
2023-07-26 19:55:46 +0800

    fix:update path

--- src/code/source-code/embedding/app/main.py
+++ src/code/source-code/embedding/app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 import uvicorn
 from schema import EmbeddingRequest, EmbeddingResponse, SimilarityResponse
 import service
 app = FastAPI()
 
+@app.get("/", include_in_schema=False)
+async def redirect_to_docs():
+    response = RedirectResponse(url='/docs')
+    return response
 
 @app.post("/encode", response_model=EmbeddingResponse)
 async def encode(request: EmbeddingRequest):
