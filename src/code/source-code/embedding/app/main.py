0149c1157895e8bc87c132983fc0a605450a3a95
布尔 <zhengyanchong.zyc@alibaba-inc.com>
2023-08-08 20:06:19 +0800

    feat: openai embedding compatible

--- src/code/source-code/embedding/app/main.py
+++ src/code/source-code/embedding/app/main.py
@@ -1,19 +1,15 @@
 from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
 import uvicorn
 from schema import EmbeddingRequest, EmbeddingResponse, SimilarityResponse
 import service
 app = FastAPI()
 
-@app.get("/", include_in_schema=False)
-async def redirect_to_docs():
-    response = RedirectResponse(url='/docs')
-    return response
 
-@app.post("/encode", response_model=EmbeddingResponse)
+@app.post("/embedding", response_model=EmbeddingResponse)
 async def encode(request: EmbeddingRequest):
     return {
-        "embeddings": service.encode(request.sentences).tolist()
+        "object": "list",
+        "embeddings": service.encode(request.sentences)
     }
 
 
